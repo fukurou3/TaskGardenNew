d06b378b7fa15aa5cad508eb30a892661f870c05
TaskGarden User <user@taskgarden.local>
2025-06-12 00:30:23 +0900

    薄暗い問題修正完了

--- lib/TimerSoundManager.ts
+++ lib/TimerSoundManager.ts
@@ -123,10 +123,10 @@ class TimerSoundManager {
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: false,
         staysActiveInBackground: false,
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        interruptionModeIOS: Audio.InterruptionModeIOS.DoNotMix,
         playsInSilentModeIOS: true,
         shouldDuckAndroid: true,
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+        interruptionModeAndroid: Audio.InterruptionModeAndroid.DoNotMix,
         playThroughEarpieceAndroid: false,
       });
     } catch (error) {
